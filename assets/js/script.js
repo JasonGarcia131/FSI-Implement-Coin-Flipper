06afed60ada5423b951a68a6e03f3d536bbf95c4
Jason Garcia <jaygarcia0530@gmail.com>
2021-12-08 12:53:46 -0800

    Added css to buttons and prompted which side coin lands on.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // TODO: Declare any global variables we need
-// This is for the page layout
 let container = document.querySelector('.container')
 let divButton = document.querySelector('.button-div')
+//css styling
 divButton.style.width = '100%'
 divButton.style.height = '60px'
 divButton.style.border = '1px solid black'
@@ -9,13 +9,6 @@ divButton.style.textAlign = 'center'
 container.append(divButton)
 let header = document.querySelector('.header')
 header.style.color = "orange"
-
-let flip = document.querySelector('.flip-button')
-// This is for visuals
-flip.style.backgroundColor = 'linear-gradiant(to left, blue, white)'
-
-
-
 
 
 let heads = document.querySelector('#penny-heads')
@@ -25,30 +18,35 @@ tails.style.display = 'none'
 let tailsCount = 0;
 let headsCount = 0;
 let numOfFlip = 0;
+
+let flip = document.querySelector('.flip-button')
+// This is for visuals
+flip.style.backgroundImage = 'linear-gradient(220deg, lightblue, white)'
+
+//Code for buttons begin
 flip.addEventListener('click', function () {
     numOfFlip++
-let pennyArr = ['heads', 'tails']
-let random = pennyArr[Math.floor(Math.random()*pennyArr.length)]
-console.log(random)
+    let pennyArr = ['heads', 'tails']
+    let random = pennyArr[Math.floor(Math.random()*pennyArr.length)]
 
     if (random == 'tails'){
      heads.style.display = 'none'
      tails.style.display = ''
+     document.querySelector('.header').textContent = 'You Flipped Tails!'
      tailsCount++
-     console.log(tailsCount)
      document.querySelector('#tails').textContent = `${tailsCount}`
      
    }
    else { 
     heads.style.display = ''
     tails.style.display = 'none'
+    document.querySelector('.header').textContent = 'You Flipped Heads!'
     headsCount++
-    console.log(headsCount)
     document.querySelector('#heads').textContent = `${headsCount}`
    }
 
    let percentTails = Math.round((tailsCount/numOfFlip)*100)
-  let percentHeads = Math.round((headsCount/numOfFlip)*100)
+    let percentHeads = Math.round((headsCount/numOfFlip)*100)
 
   let headPer = document.querySelector('#heads-percent')
   let tailsPer = document.querySelector('#tails-percent')
@@ -58,18 +56,17 @@ console.log(random)
 })
 
 let clear = document.querySelector(".clear-button")
+clear.style.backgroundImage = 'linear-gradient(220deg, gold, white)'
 
 clear.addEventListener('click', function(){
-    
-    
-    tailsCount = 0;
+        
+tailsCount = 0;
  headsCount = 0;
  numOfFlip = 0;
     document.querySelector('#heads-percent').textContent = '0%'
     document.querySelector('#tails-percent').textContent = '0%'
     document.querySelector('#heads').textContent = '0'
     document.querySelector('#tails').textContent = '0'
-
 
 })
 
@@ -78,23 +75,4 @@ clear.addEventListener('click', function(){
 
 
 
-
-    // TODO: Add event listener and handler for flip and clear buttons
-
-
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
 
